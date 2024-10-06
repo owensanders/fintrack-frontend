4cbfa0d13370f756ba5499134384fe6a8be2cf7a
Owen Sanders <owen-sanders@hotmail.co.uk>
2024-10-06 19:04:54 +0100

    Create re useable input component for forms

---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mb-4">
+    <label :for="id" class="font-medium">{{ label }}</label>
+    <input
+      :type="type"
+      :id="id"
+      :value="modelValue"
+      @input="updateValue"
+      required
+      class="mt-1 w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm p-1"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps<{
+  label: string;
+  id: string;
+  modelValue: string;
+  type: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+const updateValue = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+};
+</script>
--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="bg-gray-800 shadow-md rounded-lg p-8 w-full max-w-md">
     <h2 class="text-2xl font-semibold mb-6 text-center">Login</h2>
-
     <form @submit.prevent="handleLogin">
-      <div class="mb-4">
-        <label for="email" class="font-medium">Email</label>
-        <input
-          type="email"
-          id="email"
-          v-model="email"
-          required
-          class="mt-1 w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm p-1"
-        />
-      </div>
-      <div class="mb-4">
-        <label for="password" class="font-medium">Password</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          required
-          class="mt-1 w-full border-gray-600 bg-gray-700 text-white rounded-md shadow-sm p-1"
-        />
-      </div>
+      <Input label="Email" id="email" type="email" v-model="email" required />
+      <Input
+        label="Password"
+        id="password"
+        type="password"
+        v-model="password"
+        required
+      />
       <button
         type="submit"
         class="w-full py-2 px-4 bg-gray-900 hover:bg-gray-700 text-white font-semibold rounded-md shadow transition duration-150"
@@ -41,6 +28,7 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import Input from "@/components/Input.vue";
 
 const email = ref("");
 const password = ref("");

de12d304243d5fff488b2c3ba82ead121afd173a
Owen <osanders@PRE3571-F8ZTSL3.hhf.local>
2024-12-13 10:05:52 +0000

    Tweak error handling to also use message as sometimes errors aren't returned

--- src/utils/formHelper.ts
+++ src/utils/formHelper.ts
@@ -5,9 +5,9 @@ export const resetFormErrors = (errors: Record<string, string[]>): void => {
     });
 };
 
-// Processes and returns validation errors from an error response
 export const processFormErrors = (error: any): Record<string, string[]> => {
     const errors: Record<string, string[]> = {};
+
     if (error?.response?.status === 422 && error.response.data?.errors) {
         Object.keys(error.response.data.errors).forEach((key) => {
             errors[key] = error.response.data.errors[key];
@@ -26,6 +26,10 @@ export const processFormErrors = (error: any): Record<string, string[]> => {
         } else {
             errors.general = ["There was an error when trying to make this request, please try again."];
         }
+    }
+
+    if (!Object.keys(errors).length && error?.response?.data?.message) {
+        errors.general = [error.response.data.message];
     }
 
     return errors;
